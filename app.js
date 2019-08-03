44c97b2f4ff75258ae86d82ba8716811031ce1e1
sergio-esteban <sergio.smiling@gmail.com>
2019-08-03 17:29:27 -0500

    Express-Sessions Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,14 +31,24 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+// my session middleware is all set up
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 // enough to implement basic Authentication within our application
 function auth(req, res, next) {
   // console.log(req.headers);
-  console.log(req.signedCookies)
+  // console.log(req.signedCookies)
+  console.log(req.session)
   // if the signed cookie doesn’t contain the user property on it
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
 
     if (!authHeader) {
@@ -57,7 +69,8 @@ function auth(req, res, next) {
 
     if (user == 'admin' && pass == 'password') {
       // 'user' is the name, 'admin' is the value
-      res.cookie('user', 'admin', { signed: true });
+      // res.cookie('user', 'admin', { signed: true });
+      req.session.user = 'admin';
       next(); // authorized
       // from the auth, this request will passed on the next set of middleware
       // then Express will try to match the specific request to were
@@ -69,7 +82,7 @@ function auth(req, res, next) {
     }
   }
   else {
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       next();
     }
     else {
