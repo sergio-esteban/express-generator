1948ef92b419ff70902c8be03b80271d24b5ec79
Sergio Esteban <sergio.smiling@gmail.com>
2019-08-17 21:52:20 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// importing node modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +7,7 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+// using express to route the pages
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter')
@@ -16,6 +18,7 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
+// setting up the url to the db
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 connect.then((db) => {
@@ -33,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
+// setting up the session middleware with the various options
 // my session middleware is all set up
 app.use(session({
   name: 'session-id',
@@ -42,63 +46,63 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 // enough to implement basic Authentication within our application
+// Authentication function
 function auth(req, res, next) {
   // console.log(req.headers);
   // console.log(req.signedCookies)
-  console.log(req.session)
+  console.log(req.session);
   // if the signed cookie doesn’t contain the user property on it
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      var err = new Error('Dude You are not Authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    // the buffer enables you to split the value and
-    // we also get the encoding of the buffer which is Base64
-    // we are looking for only the second element of this array
-    // the split will cause the string to split into an array of 2 items
-    // we are picking up the base 64 encoded string from that
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-
-    if (user == 'admin' && pass == 'password') {
-      // 'user' is the name, 'admin' is the value
-      // res.cookie('user', 'admin', { signed: true });
-      req.session.user = 'admin';
-      next(); // authorized
-      // from the auth, this request will passed on the next set of middleware
-      // then Express will try to match the specific request to were
-    } else {
-      var err = new Error('You are not authenticated yet!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error('Dude You are not Authenticated!');
+    // res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
   }
+  // the buffer enables you to split the value and
+  // we also get the encoding of the buffer which is Base64
+  // we are looking for only the second element of this array
+  // the split will cause the string to split into an array of 2 items
+  // we are picking up the base 64 encoded string from that
+  // var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  // var user = auth[0];
+  // var pass = auth[1];
+
+  // if (user === 'admin' && pass === 'password') {
+  // 'user' is the name, 'admin' is the value
+  // res.cookie('user', 'admin', { signed: true });
+  // req.session.user = 'admin';
+  // next(); // authorized
+  // from the auth, this request will passed on the next set of middleware
+  // then Express will try to match the specific request to were
+  //   } else {
+  //     var err = new Error('You are not authenticated yet!');
+  //     res.setHeader('WWW-Authenticate', 'Basic');
+  //     err.status = 401;
+  //     return next(err);
+  //   }
+  // }
   else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
+      // console.log('req.session: ', req.session);
       next();
     }
     else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
     }
   }
 }
 
 app.use(auth);
 
+//  enables to server static data from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,109 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+
 var router = express.Router();
 
+router.use(bodyParser.json());
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
+// to signup a new user /user/signup
+router.post('/signup', (req, res, next) => {
+  // the client will include json string with username and password properties
+  User.findOne({ username: req.body.username })
+    .then((user) => {
+      // so you don't allow duplicate user for signing up
+      if (user != null) {
+        var err = new Error('User ' + req.body.username + ' already exists!')
+        err.status = 403; //forbidden code
+        next(err);
+      }
+      else {
+        return User.create({
+          username: req.body.username,
+          password: req.body.password
+        });
+      }
+    })
+    .then((user) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json')
+      res.json({ status: 'Registration Successful!', user: user })
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+// to login a new user
+router.post('/login', (req, res, next) => {
+  // it means that the user has not yet authenticated himself.
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      var err = new Error('Dude You are not Authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+
+
+    User.findOne({ username: username })
+      .then((user) => {
+        // which means tht we couldn't find a user with that particular username:
+        if (user === null) {
+          var err = new Error('User ' + username + 'does not exists!');
+          err.status = 403; //forbidden code
+          return next(err);
+        }
+        else if (user.password !== password) {
+          var err = new Error('Your password is incorrect');
+          err.status = 403;
+          return next(err);
+        }
+        else if (user.username === 'username' && user.password === 'password') {
+          req.session.user = 'authenticated';
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain')
+          res.end('You are authenticated')
+        }
+      })
+      .catch((err) => next(err))
+  }
+  else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('You are already authenticated')
+  }
+})
+
+// why we do a get on the logout rather than a post which we did in the login?
+// in login you need to submit password and username for logout you simply logging out yourself from the system
+// the server already is tracking you based upon your session id and inside that session cookie
+
+router.get('/logout', (req, res) => {
+  // session must exist
+  if (req.session) {
+    // the session is destroyed and the information is removed from the server side
+    req.session.destroy();
+    // we need a method of deleting the cookie that is stored on the client side
+    // and also the client cannot be used in expired session to try to contact that server
+    res.clearCookie('session-id') //the cookie name is session-id
+    // redirected to the home page here
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in!');
+    err.status = 403; //forbidden operation
+    next(err);
+  }
+})
+
 
 module.exports = router;
